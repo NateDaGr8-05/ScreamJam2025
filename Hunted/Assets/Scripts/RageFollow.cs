670edbe4ade1829a8766a7c763d65981f17e8002
Cristain Abate <caa4724@g.rit.edu>
2025-10-17 02:56:36 -0400

    rage bar follow

--- Hunted/Assets/Scripts/RageFollow.cs
+++ Hunted/Assets/Scripts/RageFollow.cs
@@ -13,6 +13,6 @@ public class CameraFollow : MonoBehaviour
     void Update()
     {
         //follows player
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + 3, 0);
+        transform.position = new Vector3(player.transform.position.x + 5, player.transform.position.y + 7, 0);
     }
 }
